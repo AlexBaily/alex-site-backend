3a214751592ca1f708a4f96f83ab7675f904c03d
AlexBaily <alex.d.baily@gmail.com>
2019-03-24 18:36:07 +0000

    Add initial commit of main.go file

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
+
+)
+
+#Http handler for responding to http/s requests.
+func roothandler(w http.ResponseWriter, r *http.Request) {
+	#Set response headers.
+	w.Header().Add("statusDescription", "200 OK")
+	w.Header().Set("statusDescription", "200 OK")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	URISegments := strings.Split(r.URL.Path, "/")
+	w.Write([]byte(URISegments[1]))
+}
+
+func main() {
+	#Create a new mux router.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", roothandler)
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
