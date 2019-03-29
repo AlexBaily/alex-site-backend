118d7bd86d2b78a43c5514ec9d80b3ba89a42153
AlexBaily <alex.d.baily@gmail.com>
2019-03-29 20:24:40 +0000

    Add the start of the code for querying DynamoDB

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"log"
 	"net/http"
 	"strings"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 //Http handler for responding to http/s requests.
-func roothandler(w http.ResponseWriter, r *http.Request) {
+func rootHandler(w http.ResponseWriter, r *http.Request) {
 	//Set response headers.
 	w.Header().Add("statusDescription", "200 OK")
 	w.Header().Set("statusDescription", "200 OK")
@@ -17,9 +20,33 @@ func roothandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(URISegments[1]))
 }
 
+func fullScan() {
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+	    SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := dynamodb.New(sess)
+
+	// Get the list of tables 
+	//Lifted from the AWS docs - This just gets a list of tables
+	result, err := svc.ListTables(&dynamodb.ListTablesInput{})
+	if err != nil {
+	 fmt.Println(err)
+	}
+
+	fmt.Println("Tables:")
+	fmt.Println("")
+
+	for _, n := range result.TableNames {
+	    fmt.Println(*n)
+	}
+
+}
+
 func main() {
 	//Create a new mux router.
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", roothandler)
+	mux.HandleFunc("/", rootHandler)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
