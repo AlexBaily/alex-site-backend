58d5fee3afffcb4336190fdd8518db2de9639ccd
AlexBaily <alex.d.baily@gmail.com>
2019-04-06 19:28:01 +0000

    Add the scanning of the workouts in the DynamoDB table

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
-	"strings"
+	"fmt"
 
-//	"github.com/aws/aws-sdk-go/aws/session"
-//	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
 )
 
 //Http handler for responding to http/s requests.
@@ -17,11 +21,11 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	w.Write([]byte("{key:value}"))
+	w.Write([]byte(fullScan()))
 }
 
-/*
-func fullScan() {
+
+func fullScan()(tables string) {
 
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 	    SharedConfigState: session.SharedConfigEnable,
@@ -29,25 +33,64 @@ func fullScan() {
 
 	svc := dynamodb.New(sess)
 
+	//Filter and Projection are required for the expression builder.
+	filter := expression.Name("UserID").Equal(expression.Value("1"))
+	projection := expression.NamesList(
+		expression.Name("WorkoutTimeStamp"),
+		expression.Name("Exercise"),
+	)
+	expr, err := expression.NewBuilder().
+		WithFilter(filter).
+		WithProjection(projection).
+		Build()
+	if err != nil {
+		fmt.Println(err)
+	}
+	//Load up the parameters into a struct
+	params := &dynamodb.ScanInput{
+	ExpressionAttributeNames:  expr.Names(),
+	ExpressionAttributeValues: expr.Values(),
+	FilterExpression:          expr.Filter(),
+	ProjectionExpression:      expr.Projection(),
+	TableName:                 aws.String("workouts"),
+	}
+
 	// Get the list of tables 
 	//Lifted from the AWS docs - This just gets a list of tables
-	result, err := svc.ListTables(&dynamodb.ListTablesInput{})
+	result, err := svc.Scan(params)
 	if err != nil {
 	 fmt.Println(err)
 	}
+	//Used to check what keys are returned from the table scan
+	/* 
+	for keys := range tablesMap {
+		tables += keys
+	}*/
 
-	fmt.Println("Tables:")
-	fmt.Println("")
-
-	for _, n := range result.TableNames {
-	    fmt.Println(*n)
+	type Record struct {
+	    Exercise   string
 	}
 
-}*/ 
+
+	recs := []Record{}
+
+	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &recs)
+	if err != nil {
+         panic(fmt.Sprintf("failed to unmarshal Dynamodb Scan Items, %v", err))
+	}
+
+
+	tables = recs[0].Exercise
+	//log.Printf("records %+v", result)
+	//log.Printf("records %+v", recs[0].Exercise)
+	return
+
+}
 
 func main() {
 	//Create a new mux router.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", rootHandler)
+	fullScan()
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
