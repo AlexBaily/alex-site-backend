7c157ed0cc7febc1ccf8124d0a14a6c8bf17a255
AlexBaily <alex.d.baily@gmail.com>
2019-04-29 19:11:24 +0000

    Change full scan to query for a select user; add start of middleware for authentication.

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"encoding/json"
 	"os"
 
+	"github.com/gorilla/mux"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
@@ -15,16 +17,26 @@ import (
 
 )
 
+//"Global" variable for the exerise table.
 var (
 	exrtable string = os.Getenv("EXRTABLE")
 )
 
+//Record struct that will house the DynamoDB records.
 type Record struct {
 	UserID       string
 	ExerciseName string
 	ExerciseDate string
 	Weight       int
 	Reps         int
+}
+
+//Middleware to read the Authorization header for the Cognito JWT token
+func authMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w,r)
+	}
+
 }
 
 //Http handler for responding to http/s requests.
@@ -35,75 +47,84 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	w.Write([]byte(fullScan()))
+	w.Write([]byte("{\"Route\":\"Test\"}"))
 }
 
+func exerciseHandler(w http.ResponseWriter, r *http.Request) {
+        //Set response headers.
+        w.Header().Add("statusDescription", "200 OK")
+        w.Header().Set("statusDescription", "200 OK")
+        w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusOK)
+	log.Printf("records %+v", queryTable("0000"))
+	//retrieve the UserID variable
 
-func fullScan()(tables []byte) {
+        w.Write(queryTable("0000"))
+}
 
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-	    SharedConfigState: session.SharedConfigEnable,
-	}))
+func queryTable(UserID string)(queryJson []byte) {
+        sess := session.Must(session.NewSessionWithOptions(session.Options{
+            SharedConfigState: session.SharedConfigEnable,
+        }))
 
-	svc := dynamodb.New(sess)
+        svc := dynamodb.New(sess)
 
-	//Filter and Projection are required for the expression builder.
-	filter := expression.Name("UserID").Equal(expression.Value("1"))
-	projection := expression.NamesList(
-		expression.Name("WorkoutTimeStamp"),
-		expression.Name("Exercise"),
-	)
-	expr, err := expression.NewBuilder().
-		WithFilter(filter).
-		WithProjection(projection).
-		Build()
-	if err != nil {
-	 fmt.Println(err)
-	}
-	//Load up the parameters into a struct
-	params := &dynamodb.ScanInput{
-	 ExpressionAttributeNames:  expr.Names(),
-	 ExpressionAttributeValues: expr.Values(),
-	 FilterExpression:          expr.Filter(),
-	 ProjectionExpression:      expr.Projection(),
-	 TableName:                 aws.String(exrtable),
-	}
+        //Filter and Projection are required for the expression builder.
+        filter := expression.Name("UserID").Equal(expression.Value(UserID))
+        projection := expression.NamesList(
+                expression.Name("ExerciseName"),
+                expression.Name("ExerciseDate"),
+		expression.Name("Weight"),
+		expression.Name("Reps"),
+        )
+        expr, err := expression.NewBuilder().
+                WithFilter(filter).
+                WithProjection(projection).
+                Build()
+        if err != nil {
+         fmt.Println(err)
+        }
+        //Load up the parameters into a struct
+        params := &dynamodb.ScanInput{
+         ExpressionAttributeNames:  expr.Names(),
+         ExpressionAttributeValues: expr.Values(),
+         FilterExpression:          expr.Filter(),
+         ProjectionExpression:      expr.Projection(),
+         TableName:                 aws.String(exrtable),
+        }
 
-	//Complete a scan of the table with the params from above
-	result, err := svc.Scan(params)
-	if err != nil {
-	 fmt.Println(err)
-	}
-	//Used to check what keys are returned from the table scan
-	/* 
-	for keys := range tablesMap {
-		tables += keys
-	}*/
+        //Complete a scan of the table with the params from above
+        result, err := svc.Scan(params)
+        if err != nil {
+         fmt.Println(err)
+        }
+        //Used to check what keys are returned from the table scan
+        /* 
+        for keys := range tablesMap {
+                tables += keys
+        }*/
 
+        recs := []Record{}
 
-
-	recs := []Record{}
-
-	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &recs)
-	if err != nil {
+        err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &recs)
+        if err != nil {
          panic(fmt.Sprintf("failed to unmarshal Dynamodb Scan Items, %v", err))
-	}
+        }
 
-	//Marshal the records into JSON
-	tables, err = json.Marshal(recs[0])
-	if err != nil {
-	 panic(fmt.Sprintf("failed to marshal records, %v", err))
-	}
-	//log.Printf("records %+v", result)
-	//log.Printf("records %+v", recs[0].Exercise)
-	return
+        //Marshal the records into JSON
+        queryJson, err = json.Marshal(recs[0])
+        if err != nil {
+         panic(fmt.Sprintf("failed to marshal records, %v", err))
+        }
+        log.Printf("records %+v", recs[0])
+        return
 
 }
 
 func main() {
 	//Create a new mux router.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", rootHandler)
-	fullScan()
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	r := mux.NewRouter()
+	r.HandleFunc("/", rootHandler)
+	r.HandleFunc("/exercises", exerciseHandler)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
